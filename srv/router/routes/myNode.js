b9c68e1ab16af29625622f50afae64553ce79e48
Ferry Djaja <ferrygun@yahoo.com>
2020-03-07 14:04:31 +0800

    Update myNode.js

--- srv/router/routes/myNode.js
+++ srv/router/routes/myNode.js
@@ -7,6 +7,13 @@ const https = require('https');
 const csv = require('csvtojson')
 const GeoJSON = require('geojson')
 
+const confirmed =
+	'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
+const death =
+	'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv';
+const recovered =
+	'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv';
+
 function formatDate(d) {
 	//get the month
 	var month = d.getMonth();
@@ -37,11 +44,11 @@ function formatDate(d) {
 	return month + '/' + day + '/' + year;
 }
 
-function readHeader() {
+function readData(url, flag) {
 	return new Promise(resolve => {
 		const result = [];
 		https.get(
-			'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+			url,
 			(resp) => {
 				let data = '';
 
@@ -67,122 +74,9 @@ function readHeader() {
 									'Province/State': csvRow[i]['Province/State'],
 									'Country/Region': csvRow[i]['Country/Region'],
 									'Lat': csvRow[i]['Lat'],
-									'Long': csvRow[i]['Long']
+									'Long': csvRow[i]['Long'],
+									[flag]: csvRow[i][date]
 								})
-							}
-							resolve(result);
-						})
-				});
-
-			}).on("error", (err) => {
-			console.log("Error: " + err.message);
-		});
-	});
-}
-
-function readConfirmed() {
-	return new Promise(resolve => {
-		const result = [];
-		https.get(
-			'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
-			(resp) => {
-				let data = '';
-
-				// A chunk of data has been recieved.
-				resp.on('data', (chunk) => {
-					data += chunk;
-				});
-
-				// The whole response has been received. Print out the result.
-				resp.on('end', () => {
-					csv({
-							noheader: false,
-							output: "json"
-						})
-						.fromString(data)
-						.then((csvRow) => {
-							const result = [];
-							var d = new Date();
-							var date = formatDate(d);
-							console.log(date);
-							for (var i = 0; i < csvRow.length; i++) {
-								result.push(csvRow[i][date]);
-							}
-							resolve(result);
-						})
-				});
-
-			}).on("error", (err) => {
-			console.log("Error: " + err.message);
-		});
-	});
-}
-
-function readDeath() {
-	return new Promise(resolve => {
-		const result = [];
-		https.get(
-			'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv',
-			(resp) => {
-				let data = '';
-
-				// A chunk of data has been recieved.
-				resp.on('data', (chunk) => {
-					data += chunk;
-				});
-
-				// The whole response has been received. Print out the result.
-				resp.on('end', () => {
-					csv({
-							noheader: false,
-							output: "json"
-						})
-						.fromString(data)
-						.then((csvRow) => {
-							const result = [];
-							var d = new Date();
-							var date = formatDate(d);
-							console.log(date);
-							for (var i = 0; i < csvRow.length; i++) {
-								result.push(csvRow[i][date]);
-							}
-							resolve(result);
-						})
-				});
-
-			}).on("error", (err) => {
-			console.log("Error: " + err.message);
-		});
-	});
-}
-
-function readRecovered() {
-	return new Promise(resolve => {
-		const result = [];
-		https.get(
-			'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv',
-			(resp) => {
-				let data = '';
-
-				// A chunk of data has been recieved.
-				resp.on('data', (chunk) => {
-					data += chunk;
-				});
-
-				// The whole response has been received. Print out the result.
-				resp.on('end', () => {
-					csv({
-							noheader: false,
-							output: "json"
-						})
-						.fromString(data)
-						.then((csvRow) => {
-							const result = [];
-							var d = new Date();
-							var date = formatDate(d);
-							console.log(date);
-							for (var i = 0; i < csvRow.length; i++) {
-								result.push(csvRow[i][date]);
 							}
 							resolve(result);
 						})
@@ -200,22 +94,21 @@ module.exports = function () {
 
 	app.get("/getSessionInfo", (req, res) => {
 		async function msg() {
-			const header_msg = await readHeader();
-			const confirmed_msg = await readConfirmed();
-			const death_msg = await readDeath();
-			const recovered_msg = await readRecovered();
+			const confirmed_msg = await readData(confirmed, "confirmed");
+			const death_msg = await readData(death, "death");
+			const recovered_msg = await readData(recovered, "recovered");
 
 			const result = [];
 
-			for (var i = 0; i < header_msg.length; i++) {
+			for (var i = 0; i < confirmed_msg.length; i++) {
 				result.push({
-					'Province/State': header_msg[i]['Province/State'],
-					'Country/Region': header_msg[i]['Country/Region'],
-					'Lat': parseFloat(header_msg[i]['Lat']),
-					'Long': parseFloat(header_msg[i]['Long']),
-					'Recovered': parseFloat(recovered_msg[i]),
-					'Confirmed': parseFloat(confirmed_msg[i]),
-					'Death': parseFloat(death_msg[i])
+					'Province/State': confirmed_msg[i]['Province/State'],
+					'Country/Region': confirmed_msg[i]['Country/Region'],
+					'Lat': parseFloat(confirmed_msg[i]['Lat']),
+					'Long': parseFloat(confirmed_msg[i]['Long']),
+					'Recovered': parseFloat(recovered_msg[i]['recovered']),
+					'Confirmed': parseFloat(confirmed_msg[i]['confirmed']),
+					'Death': parseFloat(death_msg[i]['death'])
 				})
 			}
 
